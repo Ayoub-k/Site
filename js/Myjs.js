17d32d3d32ec32a0c4a2863a616ad4cbbcb17823
Ayoub-k <ELKHADDOURI@outlook.fr>
2022-01-12 08:25:37 +0100

    padination with jqeury

--- js/Myjs.js
+++ js/Myjs.js
@@ -24,96 +24,62 @@ switch(categorie){
 
 $('#wrapper').pagination({
     dataSource: [...Array(100).keys()],
-    pageSize: 12,
+    pageSize: 9,
     formatResult: function(data) {
-        console.log(data);
-        for (var i = 0, len = data.length; i < len; i++) {
 
-            data +=`
-            
+        var data = `
             <div class="product-sec1 px-sm-4 px-3 py-sm-5  py-3 mb-4">
-            <h3 class="heading-tittle text-center font-italic">New Brand Mobiles</h3>
-            <div class="row">
+                <h3 class="heading-tittle text-center font-italic">New Brand Mobiles</h3>
+        `;
+
+        let i=0, countItem=0;
+        while(i < data.length && countItem < 3){
+            data += `<div class="row">`;
+            let j=0;
+            while(j < 3){
+                data += `
                 <div class="col-md-4 product-men mt-5">
-                    <div class="men-pro-item simpleCart_shelfItem">
-                        <div class="men-thumb-item text-center">
-                            <img src="images/m1.jpg" alt="">
-                            <div class="men-cart-pro">
-                                <div class="inner-men-cart-pro">
-                                    <a href="single.html" class="link-product-add-cart">Quick View</a>
-                                </div>
+                <div class="men-pro-item simpleCart_shelfItem">
+                    <div class="men-thumb-item text-center">
+                        <img src="images/ImagesProducts/Chargeur/${i+j}.jpg" alt="">
+                        <div class="men-cart-pro">
+                            <div class="inner-men-cart-pro">
+                                <a href="single.html" class="link-product-add-cart">Quick View</a>
                             </div>
-                        </div>
-                        <div class="item-info-product text-center border-top mt-4">
-                            <h4 class="pt-1">
-                                <a href="single.html">Samsung Galaxy J7</a>
-                            </h4>
-                            <div class="info-product-price my-2">
-                                <span class="item_price">$200.00</span>
-                                <del>$280.00</del>
-                            </div>
-                            
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4 product-men mt-5">
-                    <div class="men-pro-item simpleCart_shelfItem">
-                        <div class="men-thumb-item text-center">
-                            <img src="/ImagesProducts/Chargeur/${i}.jpg" alt="">
-                            <div class="men-cart-pro">
-                                <div class="inner-men-cart-pro">
-                                    <a href="single.html" class="link-product-add-cart">Quick View</a>
-                                </div>
-                            </div>
-                            <span class="product-new-top">New</span>
-
+                    <div class="item-info-product text-center border-top mt-4">
+                        <h4 class="pt-1">
+                            <a href="single.html">Samsung Galaxy J7</a>
+                        </h4>
+                        <div class="info-product-price my-2">
+                            <span class="item_price">$200.00</span>
+                            <del>$280.00</del>
                         </div>
-                        <div class="item-info-product text-center border-top mt-4">
-                            <h4 class="pt-1">
-                                <a href="single.html">OPPO A37f</a>
-                            </h4>
-                            <div class="info-product-price my-2">
-                                <span class="item_price">$230.00</span>
-                                <del>$250.00</del>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 product-men mt-5">
-                    <div class="men-pro-item simpleCart_shelfItem">
-                        <div class="men-thumb-item text-center">
-                            <img src="images/m3.jpg" alt="">
-                            <div class="men-cart-pro">
-                                <div class="inner-men-cart-pro">
-                                    <a href="single.html" class="link-product-add-cart">Quick View</a>
-                                </div>
-                            </div>
-                            <span class="product-new-top">New</span>
-
-                        </div>
-                        <div class="item-info-product text-center border-top mt-4">
-                            <h4 class="pt-1">
-                                <a href="single.html">Apple iPhone X</a>
-                            </h4>
-                            <div class="info-product-price my-2">
-                                <span class="item_price">$280.00</span>
-                                <del>$300.00</del>
-                            </div>
-                            
-                        </div>
+                        
                     </div>
                 </div>
             </div>
-        </div>
-            
-            `;
+                `;
+                j++
+            }
+            i++;
+            countItem++;
         }
+        data += `
+                    </div>
+                </div>
+            </div>
+        `;
+        $('#wrapper').html(data);
     },
     callback: function(data, pagination) {
+        // console.log(data);
+        console.log(pagination);
         // template method of yourself
-        var html = template(data);
-        dataContainer.html(html);
+        // var html = template(data);
+        // dataContainer.html(html);
+        // $('#wrapper').html(data);
     }
 })
 
